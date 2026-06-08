ca48726d23d50aaf184ad6aa7b446ac6c83c1417
Nishkarsh <Nishkarsh.7078@gmail.com>
2026-06-08 15:18:26 +0530

    remove lane status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { HardwareEquipmentComponent } from './components/hardware-equipment/hardware-equipment.component';
 import { LiveViewComponent } from './components/live-view/live-view.component';
-import { LaneStatusComponent } from './components/lane-status/lane-status.component';
+
 import { DataViewComponent } from './components/data-view/data-view.component';
 
 @Component({
@@ -12,7 +12,7 @@ import { DataViewComponent } from './components/data-view/data-view.component';
     HeaderComponent,
     HardwareEquipmentComponent,
     LiveViewComponent,
-    LaneStatusComponent,
+
     DataViewComponent,
   ],
   templateUrl: './app.component.html',
